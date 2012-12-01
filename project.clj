a2574f4ed05ba0f5eb73d08c7e6f637086f06713
rquintas <rquintas@rquintas.net>
2012-12-01 17:35:53 +0000

    Fix project source-path

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :plugins [[lein-cljsbuild "0.2.9"]]
   :cljsbuild {
-      :builds [{:source-path "src-cljs"
+      :builds [{:source-path "src/cljs"
                 :compiler {:output-to "main.js"
                            :optimizations :whitespace
                            :externs ["libs/d3/d3.js"]
